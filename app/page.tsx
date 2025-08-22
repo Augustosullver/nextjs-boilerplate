8451630366ec2fe6de00ef5a491994aeb7edfd0a
Augustosullver <158534817+Augustosullver@users.noreply.github.com>
2025-08-22 12:10:26 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <Image
           className="dark:invert"
           src="/next.svg"
-          alt="Next.js logo"
+          alt="BRASIL PLAY RED logo"
           width={180}
           height={38}
           priority
@@ -28,7 +28,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://discord.gg/2FvPEJaB9T"
             target="_blank"
             rel="noopener noreferrer"
           >
